c63da80445263eea76e19fd4be2f546d4a46bfe8
Beastplay1 <91196171+Beastplay1@users.noreply.github.com>
2021-10-25 19:04:14 +0400

    Add files via upload

--- home.php
+++ home.php
@@ -1,1 +1,13 @@
+<?php 
 
+session_start();
+if (!isset($_SESSION['user'])) {
+	header('location:index.php');
+	die();
+}
+echo "Hello ".$_SESSION['user'];
+ ?>
+
+<a href="logout.php">Log out</a>
+<input type="text">
+<button id="add">Add</button>
--- index.php
+++ index.php
@@ -1,7 +1,16 @@
 <h1>Login</h1>
-<form action="home.php" method="post" enctype="multipart/form-data"> 
+<form action="login.php" method="post"> 
  	<input type="text" name="login"><br><br>
  	<input type="text" name="password"><br><br>
  	<input type="submit" name="send" value="send"><br>
 </form>
 <p>If you're not signed , please click <a href="register_form.php">here</a>.</p>
+
+
+<?php 
+
+session_start();
+
+if (isset($_SESSION['error'])) {
+    echo $_SESSION['error'];
+}
