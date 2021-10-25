dc6c6613ea47cd21e9396210d6a657cdb0e8a215
Beastplay1 <91196171+Beastplay1@users.noreply.github.com>
2021-10-25 15:24:26 +0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,7 @@
+<h1>Login</h1>
+<form action="home.php" method="post" enctype="multipart/form-data"> 
+ 	<input type="text" name="login"><br><br>
+ 	<input type="text" name="password"><br><br>
+ 	<input type="submit" name="send" value="send"><br>
+</form>
+<p>If you're not signed , please click <a href="register_form.php">here</a>.</p>
