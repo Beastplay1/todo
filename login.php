bdb7bbdc76cffe500c96eb6f1a9b763ed6cd91da
Beastplay1 <91196171+Beastplay1@users.noreply.github.com>
2021-10-25 19:02:11 +0400

    Add files via upload

--- login.php
+++ login.php
@@ -1,1 +1,43 @@
+<?php 
 
+session_start();
+$errors = array();
+
+
+if (empty($_POST['login'])) {
+	$_SESSION['error']  = 'Enter your Login!';
+	header('location:index.php');
+		die();
+}
+if (empty($_POST['password'])) {
+	$_SESSION['error']  = 'Enter your Password!';
+	header('location:index.php');
+		die();
+}
+
+$login = $_POST['login'];
+$password = $_POST['password'];
+
+
+$array = file('users.php');
+
+
+foreach($array as $val){
+	$array1 = explode(" ", $val);
+	if($array1[0] == $login && $array1[1] == $password){
+		$_SESSION['user'] = $array1[2];
+		header('location:home.php');
+		die();
+	}
+}
+
+$_SESSION['error'] = "Wrong login or password";  
+
+
+header('location:index.php');
+die();
+
+
+
+
+ ?>
--- register.php
+++ register.php
@@ -1,23 +1,52 @@
 <?php 		
+session_start();
+$errors = array();
 
-// print_r($_FILES);
-// print_r($_POST);
+if (empty($_POST['login'])) {
+	$errors['login'] = 'Enter your Login!';
+}
+if (empty($_POST['password'])) {
+	$errors['password'] = 'Enter your Password!';
+}
+if (empty($_POST['passwordConf'])) {
+	$errors['conf'] = 'Enter your Password!';
+}
+if (empty($_POST['email'])) {
+	$errors['email'] = 'Enter your Email!';
+}
+$login = $_POST['login'];
+$email = $_POST['email'];
+$password = $_POST['password'];
+$passwordConf = $_POST['passwordConf'];
 
-// $login = $_POST['login'];
-// $email = $_POST['email'];
-// $password = $_POST['password'];
-// $passwordConf = $_POST['passwordConf'];
-
-// if ($login == "" || $password == "" || $email == "" || $passwordConf == "") {
-// 	$msg = "Please fill all required fields!";
-// }
-// elseif ($password !== $passwordConf) {
-// 	$msg = "Passwords not match!";
-// }
-
-// if (!is_file('img/'.$_FILES['img']['name'] )) {
-// 	move_uploaded_file($_FILES['img']['tmp_name'], 'img/'.$_FILES['img']['name']);
-// }
+if ($password !== $passwordConf) {
+	$errors['match'] = 'Passwords not match!';
+}
+$array = file('users.php');
+foreach($array as $val){
+	$array1 = explode(" ", $val);
+	if($array1[2] == $email){
+		$errors['email1'] = "This email already exists!";
+		break;
+	}
+}
 
 
-?>
+if (!empty($errors)) {
+	$_SESSION['errors'] = $errors;
+	header('location:register_form.php');
+	die();
+}
+
+
+move_uploaded_file($_FILES['img']['tmp_name'], 'img/'.$_FILES['img']['name']);
+
+
+
+
+
+$fp = fopen('users.php', 'a');
+$str = $login." ". $password." ".$email." ".$_FILES['img']['name']."\n";
+fwrite($fp, $str);
+fclose($fp);
+header('location:index.php');
